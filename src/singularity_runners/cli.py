52b81d6b8e3ee0b77f8f661c08264fb5aa6aed0b
Robert Rosca <32569096+RobertRosca@users.noreply.github.com>
2021-01-19 13:29:24 +0100

    Huzza, fix sub-subparser help calls

--- src/singularity_runners/cli.py
+++ src/singularity_runners/cli.py
@@ -1,36 +1,27 @@
 import argparse
-from typing import Optional
 
 from . import github, gitlab
 
 
 def subparser(parser: argparse.ArgumentParser):
-    subparser = parser.add_subparsers(dest='command')
+    subparser = parser.add_subparsers(dest='provider', required=True)
 
     github.subparser(subparser.add_parser('github'))
-    # gitlab.subparser(subparser.add_parser('gitlab'))
+    subparser.add_parser('gitlab')
 
     return parser
 
 def cli():
     parser = subparser(argparse.ArgumentParser(add_help=False))
 
-    print(parser.__dir__())
-
     args, unknown = parser.parse_known_args()
-    print(f"{args=}, {unknown=}")
+
+    if args.command is None:
+        parser._actions[0].choices[args.provider].print_help()
+        return 0
+
     #  Second pass to correctly parse args that apply to all methods
     args, unknown = parser.parse_known_args(unknown, args)
-    print(f"{args=}, {unknown=}")
-
-    args_dict = vars(args).copy()
-    args_dict.pop('command')
-
-    print(args.command)
-
-    parser.print_help()
-    print(parser)
-    return 0
 
 
 if __name__ == "__main__":
